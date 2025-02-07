d698656d8ac42e45d55250c6f4c513337678593c
Fred Gleason <fredg@paravelsystems.com>
2025-02-07 11:40:28 -0500

    2025-02-07 Fred Gleason <fredg@paravelsystems.com> 	* Integrated x11vnc v0.9.16ea1 into the build.
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

---
+++ x11vnc/build_x11vnc.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# build_x11vnc.sh
+#
+# Build a modified version of x11vnc.
+#
+#   (C) Copyright 2025 Fred Gleason <fredg@paravelsystems.com>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as
+#   published by the Free Software Foundation; either version 2 of
+#   the License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the Free Software
+#   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+
+rm -rf x11vnc-0.9.16ea1
+tar -zvxf x11vnc-0.9.16ea1.tar.gz
+cd x11vnc-0.9.16ea1
+./autogen.sh --bindir=/usr/lib/x11vnc-auto
+make $DEBBUILD_MAKE_ARGS
+cp src/x11vnc ../
+cd ..
+rm -rf x11vnc-0.9.16ea1
